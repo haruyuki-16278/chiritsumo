aadf880f9e7d60901bf07f983f9ae71271a1ce3d
haruyuki_16278 <haruyuki.program@gmail.com>
2022-12-18 05:47:10 +0900

    implement profile component

--- src/app/routed/home/component/profile/profile.component.ts
+++ src/app/routed/home/component/profile/profile.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  name: string = ''
+  iconUrl : string = ''
+  otetsudaiLevel: number = 1
 
+  ngOnInit () {
+    this.name = window.localStorage.getItem('name') ?? '';
+    this.iconUrl = window.localStorage.getItem('iconUrl') ?? '';
+    this.otetsudaiLevel = Number(window.localStorage.getItem('otetsudaiLevel')) ?? 1;
+  }
 }
