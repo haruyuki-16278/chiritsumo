4b74bee85965325709436d2aea7e712e19cad832
haruyuki_16278 <haruyuki.program@gmail.com>
2022-12-18 03:41:23 +0900

    implement basic login form logics

--- src/app/routed/login/login.component.ts
+++ src/app/routed/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  /* 名前 */
+  name: string = ''
+  /* アイコン */
+  iconUrl: string = ''
+  /* 選択可能なアイコンのurlのリスト */
+  selectableIconUrlList: string[] = ['1', '2', '3']
+  /* 名の入力済みフラグ */
+  nameEntered: boolean = false
 
+  constructor (
+    private readonly router: Router
+  ) {}
+
+  ngOnInit () {}
+
+  onClickEnterNameDone () {
+    window.localStorage.setItem('name', this.name)
+    this.nameEntered = true
+  }
+
+  onClickIcon (iconUrl: string) {
+    this.iconUrl = iconUrl
+  }
+
+  onClickSelectIconDone () {
+    window.localStorage.setItem('iconUrl', this.iconUrl)
+    this.router.navigate(['/home'])
+  }
 }
--- src/app/routed/login/login.module.ts
+++ src/app/routed/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -11,6 +12,7 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule
   ]
 })
