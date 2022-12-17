415add54d211f7d115476b0661ec27bf3268a3da
FukeKazki <kazkichi0906@gmail.com>
2022-12-18 05:49:07 +0900

    アイコンを設定した

--- src/app/routed/login/login.component.ts
+++ src/app/routed/login/login.component.ts
@@ -12,7 +12,11 @@ export class LoginComponent implements OnInit {
   /* アイコン */
   iconUrl: string = ''
   /* 選択可能なアイコンのurlのリスト */
-  selectableIconUrlList: string[] = ['1', '2', '3']
+  selectableIconUrlList: string[] = [
+    'https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2372&q=80',
+    'https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80',
+    'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2376&q=80'
+  ]
   /* 名の入力済みフラグ */
   nameEntered: boolean = false
 
